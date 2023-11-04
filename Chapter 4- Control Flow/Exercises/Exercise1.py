5d7fbf47556daeda70f27ab6a82e93528776b756
Joshua Montero <joshuamontero0915@gmail.com>
2023-11-04 15:13:53 +0400

    Chapter4.py

--- Chapter 4- Control Flow/Exercises/Exercise1.py
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -2,6 +2,6 @@ alien_color = 'green'
 if alien_color == 'green':
     print("You have just earned 5 points!")
 
-alien_color = 'green'
+alien_color = 'red'
 if alien_color == 'green':
     print("You have just earned 5 points!")
