55fbddecdff8507ec8b23e246f0cd6538774d9e6
Joshua Montero <joshuamontero0915@gmail.com>
2023-11-16 12:00:51 +0400

    Chapter5.py

--- Chapter 5- Dictionaries/Exercises/Exercise2.py
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -1,10 +1,10 @@
-#Creates a dictionary called glossary storing keys and values with definitions of the keys
+# Creates a dictionary called glossary storing keys and values with definitions of the keys
 glossary = {"Variable": "In programming a variable is a memory used to allocate and store a value whether it is a number word or letter.",
     "Input": "In programming input is used to receive input from the user such as age.",
     "Print": "In programming the function print is used to print out a statement and shows its output.",
     "Output": "In programming output is what is shown when printing the input of the program. Output displays the result.",
     "List": "List in python is used to store multiple values.",}
-#In these statements we concatenate messages using the keys and using the values to print the message displaying the output values of the dictionary neatly.
+# In these statements we concatenate messages using the keys and using the values to print the message displaying the output values of the dictionary neatly.
 word = "Variable"
 print ("\n" + word.title() + ": " + glossary[word])
 
--- Chapter 5- Dictionaries/Exercises/Exercise4.py
+++ Chapter 5- Dictionaries/Exercises/Exercise4.py
@@ -1,8 +1,8 @@
-#Stores keys and values of great rivers and the countries they currently are in
+# Stores keys and values of great rivers and the countries they currently are in
 dictionary = {"Yellow River": "China",
     "Colorado River": "United States",
     "Ganges River": "India",}
-#Uses for loop to neatly present the great rivers and countries stored in the dictionary
+# Uses for loop to neatly present the great rivers and countries stored in the dictionary
 for greatriver, Country in dictionary.items():
     print ("The " + greatriver.title() + " runs through " + Country.title() + ".")
 print ("\tThese are three of the greatest rivers: ")
