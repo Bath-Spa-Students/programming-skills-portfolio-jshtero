d7160aa1c22ba52ea5ac7d54543dc883a22bc94e
Joshua Montero <joshuamontero0915@gmail.com>
2023-11-16 11:29:26 +0400

    Chapter3.py

--- Chapter 3- Data Structures/Exercises/Exercise3.py
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -1,3 +1,5 @@
+# Uses a list with the variable called cars to store different types of cars as values
 cars = ["Range Rover", "Dodge Challenger", "Ferrari", "Lamborghini"]
+# Uses for loop to print a message for each car type using concatenations
 for carlovers in cars:
     print("I would like to own a " + carlovers +" when I am older.")
